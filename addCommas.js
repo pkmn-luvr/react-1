5222ede19c57c93d021bc3a82bf79bbde77ae641
Marissa <mickles.marissa@gmail.com>
2024-04-10 21:11:54 -0400

    Completed Part 2: addCommas

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,37 @@
-function addCommas() {}
+// This exercise is suspiciously easy if I use the .toLocaleString method....so I assume that I should try NOT use it?
+//
+// function addCommas(num) {
+//     return num.toLocaleString('en-US');
+// }
 
-module.exports = addCommas;
+
+function addCommas(num) {
+    // Splits number into two segments to identify integer and decimal digits
+    let splitNum = num.toString().split(".");
+    let integer = splitNum [0];
+    let decimal = splitNum.length > 1 ? "." + splitNum[1] : "";
+    let sign = '';
+  
+    // Checks for negative num
+    if (integer.startsWith("-")) {
+      sign = '-';
+      integer = integer.slice(1);
+    }
+  
+    // Reverses integer to easily add the commas - adds commas after every 3rd digit/index of the reversed integer
+    let reversedInt = integer.split("").reverse().join("");
+    let withCommas = '';
+
+    for (let i = 0; i < reversedInt.length; i++) {
+      if (i > 0 && i % 3 === 0) {
+        withCommas += ",";
+      }
+      withCommas += reversedInt[i];
+    }
+  
+    // Reverse string back to normal and join w/ decimal part and/or negative sign
+    return sign + withCommas.split("").reverse().join("") + decimal;
+  }
+  
+  module.exports = addCommas;
+  
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,31 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("adds commas to 1234", () => {
+    expect(addCommas(1234)).toBe("1,234");
+  });
+
+  test("adds commas to 1000000", () => {
+    expect(addCommas(1000000)).toBe("1,000,000");
+  });
+
+  test("adds commas to 9876543210", () => {
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+  });
+
+  test("handles single digit", () => {
+    expect(addCommas(6)).toBe("6");
+  });
+
+  test("handles negative numbers", () => {
+    expect(addCommas(-10)).toBe("-10");
+    expect(addCommas(-5678)).toBe("-5,678");
+  });
+
+  // Bonus cases
+  test("handles numbers with decimals", () => {
+    expect(addCommas(12345.678)).toBe("12,345.678");
+    expect(addCommas(-3141592.65)).toBe("-3,141,592.65");
+  });
 });
